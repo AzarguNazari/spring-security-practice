2722624b50134a99c37fc3a1b4ecf7d787c6a5dc
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-12-13 01:02:31 +0100

    add example 10 of CORS on spring boot

--- cors-example/src/main/java/com/example/demo/CorsExampleApplication.java
+++ cors-example/src/main/java/com/example/demo/CorsExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsrfExampleApplication {
+public class CorsExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsrfExampleApplication.class, args);
+		SpringApplication.run(CorsExampleApplication.class, args);
 	}
 
 }
